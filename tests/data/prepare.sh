05fe441e5fae131e8ac577546496637b2e4c8b28
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-08-10 12:16:08 +0200

    Adding the script for gathering the testing data and updated the json test data

---
+++ tests/data/prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir Test
+cd Test/
+mkdir 005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/
+cd 005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/
+cp ../../../../data/DSD100subset/Mixtures/Test/005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/mixture.wav vocals.wav
+cp vocals.wav accompaniment.wav
