323d12b068529265bf26b94f73c283735c922851
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-08-10 12:17:17 +0200

    Added preparing the test data to travis.

--- tests/data/prepare.sh
+++ tests/data/prepare.sh
@@ -6,3 +6,4 @@ mkdir 005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/
 cd 005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/
 cp ../../../../data/DSD100subset/Mixtures/Test/005\ -\ Angela\ Thomas\ Wade\ -\ Milk\ Cow\ Blues/mixture.wav vocals.wav
 cp vocals.wav accompaniment.wav
+cd ../..
